9bf58f57b1aa6e40f95ea37ae392a83d8c6ee6ea
Sami Murtaza <sm02934@st.habib.edu.pk>
2020-03-23 18:48:49 +0500

    added model code

---
+++ Model/lin_cnn2All.py
@@ -0,0 +1,121 @@
+from torch.autograd import Variable
+from librosa.feature.inverse import mel_to_audio
+from scipy.io.wavfile import write
+
+import torch.nn as nn
+import time
+import math
+import librosa
+import numpy as np
+import torch
+import os
+
+from utils2 import *
+
+print("*IMPORTED*")
+
+style_param = 2e1
+content_param = 8e4
+learning_rate = 0.002
+
+num_epochs = 10000
+print_every = 1000
+
+cuda = True if torch.cuda.is_available() else False
+
+print("*GLOBALS SET*")
+
+styles = []
+
+basepath = "data/"
+result_path = basepath + "results/"
+
+content_source_name = "Marina"
+CONTENT_FILENAME = basepath + content_source_name + "/MS-19.WAV"
+
+style_source_name = "Marina"
+for i in os.listdir(basepath + style_source_name + "/"):
+    STYLE_FILENAME = basepath + style_source_name + "/" + i
+    styles.append(wav2spectrum2(STYLE_FILENAME)[0])
+
+print("number of style audio: ", len(styles))
+
+a_content, sr = wav2spectrum2(CONTENT_FILENAME)
+
+STYLE_FILENAME = basepath + style_source_name + style_source_name
+
+#Directory Name = 'BaseAudio_TargetAudio_epochs_learningrate'
+dir_name = result_path + CONTENT_FILENAME.split("/")[-1][:-4] + "_" + style_source_name + "_" + str(num_epochs) + "_" + str(learning_rate)
+if os.path.exists(dir_name):
+    count = 1
+    temp = dir_name + "(" +str(count) + ")"
+    while os.path.exists(temp):
+        count+=1
+        temp = dir_name + "(" +str(count) + ")"
+    dir_name = temp
+os.mkdir(dir_name)
+
+print("*DIR MADE " + dir_name +" *")
+
+a_content_torch = torch.from_numpy(a_content)[None, None, :, :]
+if cuda:
+    a_content_torch = a_content_torch.cuda()
+print(a_content_torch.shape)
+
+a_style_torch = [torch.from_numpy(i)[None, None, :, :]  for i in styles]
+
+
+if cuda:
+    a_style_torch = [i.cuda() for i in a_style_torch]
+#print(a_style_torch.shape)
+
+model = RandomCNN()
+model.eval()
+
+a_C_var = Variable(a_content_torch, requires_grad=False).float()
+a_S_var = [Variable(i, requires_grad=False).float() for i in a_style_torch]
+
+if cuda:
+    model = model.cuda()
+    a_C_var = a_C_var.cuda()
+    a_S_var = [i.cuda() for i in a_S_var]
+
+a_C = model(a_C_var)
+a_S = [model(i) for i in a_S_var]
+
+if cuda:
+    a_G_var = Variable(torch.randn(a_content_torch.shape).cuda() * 1e-3, requires_grad=True)
+else:
+    a_G_var = Variable(torch.randn(a_content_torch.shape) * 1e-3, requires_grad=True)
+optimizer = torch.optim.Adam([a_G_var])
+
+start = time.time()
+# Train the Model
+print ("*TRAINING STARTED*")
+for epoch in range(0, num_epochs + 1):
+    optimizer.zero_grad()
+    a_G = model(a_G_var)
+
+    content_loss = content_param * compute_content_loss(a_C, a_G, False)
+    style_loss = sum([style_param * compute_layer_style_loss(i, a_G, False) for i in a_S])
+    loss = content_loss + style_loss 
+    
+    loss.backward()
+    optimizer.step()
+
+    if epoch % print_every == 0:
+        gen_spectrum = a_G_var.cpu().data.numpy().squeeze()
+        gen_audio_C = dir_name + "/(" + str(epoch) + ")" + '.wav'
+        spectrum2wav(gen_spectrum, sr, gen_audio_C)
+        print("{} {}% {} content_loss:{:4f} style_loss:{:4f} total_loss:{:4f}".format(epoch,
+                                                                                      epoch / num_epochs * 100,
+                                                                                      timeSince(start),
+                                                                                      content_loss.item(),
+                                                                                      style_loss.item(), loss.item()))
+    
+
+gen_spectrum = a_G_var.cpu().data.numpy().squeeze()
+gen_audio_C = result_path + '.wav'
+spectrum2wav(gen_spectrum, sr, gen_audio_C)
+
+print("_________________________________")
---
+++ Model/utils.py
@@ -0,0 +1,185 @@
+from torch.autograd import Variable
+from librosa.feature.inverse import mel_to_audio
+from scipy.io.wavfile import write
+from librosa.util import normalize
+
+import torch.nn as nn
+import time
+import math
+import librosa
+import numpy as np
+import torch
+import os
+
+N_FFT = 256
+N_MELS = 512
+N_CHANNELS = round(1 + N_FFT/2)
+OUT_CHANNELS = 32
+
+class RandomCNN(nn.Module):
+
+    def __init__(self):
+        super(RandomCNN, self).__init__()
+
+        # 2-D CNN
+        self.conv1 = nn.Conv2d(1, OUT_CHANNELS, kernel_size=(3, 3), stride=1, padding=0)
+        self.LeakyReLU = nn.LeakyReLU(0.2)
+
+        # Set the random parameters to be constant.
+        weight = torch.randn(self.conv1.weight.data.shape)
+        self.conv1.weight = torch.nn.Parameter(weight, requires_grad=False)
+        bias = torch.zeros(self.conv1.bias.data.shape)
+        self.conv1.bias = torch.nn.Parameter(bias, requires_grad=False)
+
+    def forward(self, x_delta):
+        out = self.LeakyReLU(self.conv1(x_delta))
+        return out
+
+def wav2spectrum(filename):
+    #x, sr = librosa.load(filename)
+    #S = librosa.stft(x, N_FFT)
+    #p = np.angle(S)
+    #S = np.log1p(np.abs(S))
+    #S = librosa.filters.mel(sr=sr, n_fft=2048, n_mels=512)
+    x, sr = librosa.load(filename)
+    S = librosa.feature.melspectrogram(x, n_fft= N_FFT, n_mels = N_MELS, sr=41000)
+    return S, sr
+
+def wav2spectrum2(filename):
+    x, sr = librosa.load(filename)
+    S = librosa.stft(x, N_FFT)
+    p = np.angle(S)
+    S = np.log1p(np.abs(S))
+    return S, sr
+
+
+def spectrum2wav(spectrum, sr, outfile):
+    # Return the all-zero vector with the same shape of `a_content`
+   a = np.exp(spectrum) - 1
+   p = 2 * np.pi * np.random.random_sample(spectrum.shape) - np.pi
+   for i in range(50):
+       S = a * np.exp(1j * p)
+       x = librosa.istft(S)
+       p = np.angle(librosa.stft(x, N_FFT))
+   librosa.output.write_wav(outfile, x, sr)
+
+   # inv = librosa.feature.inverse.mel_to_audio(spectrum)
+   # write(outfile, sr, inv)
+
+def spectrum2wav2(spectrum, sr, outfile):
+    inv = librosa.feature.inverse.mel_to_audio(spectrum)
+    write(outfile, sr, inv)
+
+def wav2spectrum_keep_phase(filename):
+    x, sr = librosa.load(filename)
+    S = librosa.stft(x, N_FFT)
+    p = np.angle(S)
+
+    S = np.log1p(np.abs(S))
+    return S, p, sr
+
+
+def spectrum2wav_keep_phase(spectrum, p, sr, outfile):
+    # Return the all-zero vector with the same shape of `a_content`
+    a = np.exp(spectrum) - 1
+    for i in range(50):
+        S = a * np.exp(1j * p)
+        x = librosa.istft(S)
+        p = np.angle(librosa.stft(x, N_FFT))
+    librosa.output.write_wav(outfile, x, sr, norm=True)
+
+
+def compute_content_loss(a_C, a_G, isMel):
+    """
+    Compute the content cost
+
+    Arguments:
+    a_C -- tensor of dimension (1, n_C, n_H, n_W)
+    a_G -- tensor of dimension (1, n_C, n_H, n_W)
+
+    Returns:
+    J_content -- scalar that you compute using equation 1 above
+    """
+    m, n_C, n_H, n_W = a_G.shape
+
+    # Reshape a_C and a_G to the (m * n_C, n_H * n_W)
+    a_C_unrolled = a_C.view(m * n_C, n_H * n_W)
+    a_G_unrolled = a_G.view(m * n_C, n_H * n_W)
+
+    # Compute the cost
+    #J_content = 1.0 / (4 * m * n_C * n_H * n_W) * torch.sum((a_C_unrolled - a_G_unrolled) ** 2)
+    if isMel:
+      J_content = torch.sum((a_C_unrolled - a_G_unrolled) ** 2)
+    else:
+      J_content = 1.0 / (4 * m * n_C * n_H * n_W) * torch.sum((a_C_unrolled - a_G_unrolled) ** 2)
+
+    return J_content
+
+
+def gram(A):
+    """
+    Argument:
+    A -- matrix of shape (n_C, n_L)
+
+    Returns:
+    GA -- Gram matrix of shape (n_C, n_C)
+    """
+    GA = torch.matmul(A, A.t())
+
+    return GA
+
+
+def gram_over_time_axis(A):
+    """
+    Argument:
+    A -- matrix of shape (1, n_C, n_H, n_W)
+
+    Returns:
+    GA -- Gram matrix of A along time axis, of shape (n_C, n_C)
+    """
+    m, n_C, n_H, n_W = A.shape
+
+    # Reshape the matrix to the shape of (n_C, n_L)
+    # Reshape a_C and a_G to the (m * n_C, n_H * n_W)
+    A_unrolled = A.view(m * n_C * n_H, n_W)
+    GA = torch.matmul(A_unrolled, A_unrolled.t())
+
+    return GA
+
+
+def compute_layer_style_loss(a_S, a_G, isMel):
+    """
+    Arguments:
+    a_S -- tensor of dimension (1, n_C, n_H, n_W)
+    a_G -- tensor of dimension (1, n_C, n_H, n_W)
+
+    Returns:
+    J_style_layer -- tensor representing a scalar style cost.
+    """
+    m, n_C, n_H, n_W = a_G.shape
+
+    # Reshape the matrix to the shape of (n_C, n_L)
+    # Reshape a_C and a_G to the (m * n_C, n_H * n_W)
+
+    # Calculate the gram
+    # !!!!!! IMPORTANT !!!!! Here we compute the Gram along n_C,
+    # not along n_H * n_W. But is the result the same? No.
+    GS = gram_over_time_axis(a_S)
+    GG = gram_over_time_axis(a_G)
+
+    # Computing the loss
+    #J_style_layer = 1.0 / (4 * (n_C ** 2) * (n_H * n_W)) * torch.sum((GS - GG) ** 2)
+    if isMel:
+      J_style_layer = torch.sum((GS - GG) ** 2)
+    else:
+      J_style_layer = 1.0 / (4 * (n_C ** 2) * (n_H * n_W)) * torch.sum((GS - GG) ** 2)
+
+    return J_style_layer
+
+def timeSince(since):
+    now = time.time()
+    s = now - since
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
+
